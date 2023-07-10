e74ec9e93d1ce7e2e0a7edc0315cf529bcb9c9df
simycodes <simycodes@gmail.com>
2023-07-10 13:18:39 +0200

    up to week 12 update

--- js/main.js
+++ js/main.js
@@ -24,34 +24,34 @@ const links = [
       label: "Week-7: CSS Grid",
       url: "week-7/index.html"
     },
-    // {
-    //   label: "Week-8 notes",
-    //   url: "week-8/index.html"
-    // },
-    // {
-    //   label: "Week-9 notes",
-    //   url: "week-9/index.html"
-    // },
-    //  {
-    //   label: "Week-8 notes",
-    //   url: "week-8/index.html"
-    // },
-    // {
-    //   label: "Week-9 notes",
-    //   url: "week-9/index.html"
-    // },
-    // {
-    //   label: "Week-10 notes",
-    //   url: "week-10/index.html"
-    // },
-    // {
-    //   label: "Week-11 notes",
-    //   url: "week-11/index.html"
-    // },
-    // {
-    //   label: "Week-12 notes",
-    //   url: "week-12/index.html"
-    // },
+    {
+      label: "Week-8: notes",
+      url: "week-8/index.html"
+    },
+    {
+      label: "Week-9: notes",
+      url: "week-9/index.html"
+    },
+     {
+      label: "Week-8: Shapes(Border Radius) and Gradients",
+      url: "week-8/index.html"
+    },
+    {
+      label: "Week-9: CSS Typography",
+      url: "week-9/index.html"
+    },
+    {
+      label: "Week-10: Custom Properties and Functions",
+      url: "week-10/index.html"
+    },
+    {
+      label: "Week-11: Debugging and Optimization",
+      url: "week-11/index.html"
+    },
+    {
+      label: "Week-12: Tech Module: CSS Architecture and Component Based Design ",
+      url: "week-12/index.html"
+    },
     // {
     //   label: "Week-13 notes",
     //   url: "week-13/index.html"
